f43e1ea7b1bd155466bddef23fe46970e183047e
Nikita <nikistosov@gmail.com>
2024-09-25 20:17:08 +0200

    feat: item styling under title

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,4 +1,5 @@
-import { memo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { memo } from 'react';
 import PropTypes from 'prop-types';
 import { cn as bem } from '@bem-react/classname';
 import { numberFormat } from '../../utils';
@@ -6,15 +7,24 @@ import './style.css';
 
 function Item(props) {
   const cn = bem('Item');
+  const navigate = useNavigate();
 
   const callbacks = {
-    onAdd: e => props.onAdd(props.item._id),
+    onAdd: e => {
+      e.stopPropagation();
+      props.onAdd(props.item._id);
+    },
+    onNavigate: e => {
+      e.stopPropagation();
+      navigate(`/item/${props.item._id}`);
+    },
   };
 
   return (
     <div className={cn()}>
-      {/*<div className={cn('code')}>{props.item._id}</div>*/}
-      <div className={cn('title')}>{props.item.title}</div>
+      <div className={cn('title')} onClick={callbacks.onNavigate}>
+        {props.item.title}
+      </div>
       <div className={cn('actions')}>
         <div className={cn('price')}>{numberFormat(props.item.price)} ₽</div>
         <button onClick={callbacks.onAdd}>Добавить</button>
